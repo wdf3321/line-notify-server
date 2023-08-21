c0eeff8d303ae5ffc6f11c3c095361fcba4855e7
wdf332 <wdf332@gmail.com>
2023-08-21 11:08:04 +0800

    edit env

--- controllers/mail.js
+++ controllers/mail.js
@@ -70,3 +70,39 @@ export const sentwater = async (req, res) => {
     })
     .catch(console.error);
 };
+
+export const sentbing = async (req, res) => {
+  const jsonData = req.body;
+
+  // 處理json資料
+  console.log(jsonData);
+
+  const message = `
+    主題: ${req.body.subject} <br>
+    聯絡姓名: ${req.body.name} <br>
+    連絡電話: ${req.body.phone} <br>
+    連絡信箱: ${req.body.email} <br>
+    內容: ${req.body.content}
+  `; // Line Notify message
+
+  const transporter = nodemailer.createTransport({
+    host: 'xlinfoods.synology.me',
+    port: 25,
+    auth: {
+      user: process.env.BINGID,
+      pass: process.env.BINGPW,
+    },
+  });
+
+  transporter.sendMail({
+    from: 'chinabingcs@xlinfoods.com',
+    to: 'chinabingcs@xlinfoods.com',
+    subject: 'CHINABING-用戶來信',
+    html: `<h4>${message}</h4>`,
+  })
+    .then(info => {
+      console.log('Message sent');
+      res.status(200).send('Message sent');
+    })
+    .catch(console.error);
+};
--- route/route.js
+++ route/route.js
@@ -1,9 +1,10 @@
 import { Router } from 'express'
-import { sentwater, sentdtstw } from '../controllers/mail.js'
+import { sentwater, sentdtstw,sentbing } from '../controllers/mail.js'
 const router = Router()
 
 router.post('/dtstw', sentdtstw)
 router.post('/water', sentwater)
+router.post('/bing', sentbing)
 
 
 export default router
