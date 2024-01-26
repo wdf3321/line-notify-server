ba52e29cf66f92228c2d96da41aaac001c57122e
wdf332 <wdf332@gmail.com>
2024-01-26 15:24:41 +0800

    edit

--- controllers/mail.js
+++ controllers/mail.js
@@ -23,8 +23,8 @@ export const sentdtstw = async (req, res) => {
   });
 
   transporter.sendMail({
-    from: 'dtsmkt@dtstw.com',
-    to: 'dtsmkt@dtstw.com',
+    from: 'dtsmkt@xlinfoods.com',
+    to: 'dtsmkt@xlinfoods.com',
     subject: 'DTS-用戶來信',
     html: `<p>${message}</p>`,
   })
@@ -98,6 +98,42 @@ export const sentbing = async (req, res) => {
     from: 'chinabingcs@xlinfoods.com',
     to: 'chinabingcs@xlinfoods.com',
     subject: 'CHINABING-用戶來信',
+    html: `<h4>${message}</h4>`,
+  })
+    .then(info => {
+      console.log('Message sent');
+      res.status(200).send('Message sent');
+    })
+    .catch(console.error);
+};
+
+export const sentshanghui = async (req, res) => {
+  const jsonData = req.body;
+
+  // 處理json資料
+  console.log(jsonData);
+
+  const message = `
+    主題: ${req.body.subject} <br>
+    聯絡姓名: ${req.body.name} <br>
+    連絡電話: ${req.body.phone} <br>
+    連絡信箱: ${req.body.email} <br>
+    內容: ${req.body.content}
+  `; // Line Notify message
+
+  const transporter = nodemailer.createTransport({
+    host: 'xlinfoods.synology.me',
+    port: 25,
+    auth: {
+      user: process.env.BINGID,
+      pass: process.env.BINGPW,
+    },
+  });
+
+  transporter.sendMail({
+    from: 'chinabingcs@xlinfoods.com',
+    to: 'chinabingcs@xlinfoods.com',
+    subject: 'SHANGHUI-用戶來信',
     html: `<h4>${message}</h4>`,
   })
     .then(info => {
--- route/route.js
+++ route/route.js
@@ -1,10 +1,10 @@
 import { Router } from 'express'
-import { sentwater, sentdtstw,sentbing } from '../controllers/mail.js'
+import { sentwater, sentdtstw,sentbing ,sentshanghui} from '../controllers/mail.js'
 const router = Router()
 
 router.post('/dtstw', sentdtstw)
 router.post('/water', sentwater)
 router.post('/bing', sentbing)
-
+router.post('/shanghui', sentshanghui)
 
 export default router
